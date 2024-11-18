a98bccac438498daafbe075e30718cc5cea7e70c
liuxin <2683961691@qq.com>
2024-11-18 16:57:11 +0800

    添加多语言

--- packages/aigotools/src/lib/locales.ts
+++ packages/aigotools/src/lib/locales.ts
@@ -1,12 +1,69 @@
+//简体中文（cn）、英文（en）、法语（fr）、西班牙语（es）和德语（de）,日语、俄语、葡萄牙语、阿拉伯语、意大利语、荷兰语、韩语、印尼语、土耳其语和波兰语
 export const Languages = [
   {
     lang: "cn",
     label: "🇨🇳 简体中文",
   },
   {
+    lang: "tw",
+    label: "🇨🇳 繁體中文",
+  },
+  {
     lang: "en",
     label: "🇺🇸 English",
   },
+  {
+    lang: "fr",
+    label: "🇫🇷 Français",
+  },
+  {
+    lang: "es",
+    label: "🇪🇸 Español",
+  },
+  {
+    lang: "de",
+    label: "🇩🇪 Deutsch",
+  },
+  {
+    lang: "ja",
+    label: "🇯🇵 日本語",
+  },
+  {
+    lang: "ru",
+    label: "🇷🇺 Русский",
+  },
+  {
+    lang: "pt",
+    label: "🇵🇹 Português",
+  },
+  {
+    lang: "ar",
+    label: "🇸🇦 العربية",
+  },
+  {
+    lang: "it",
+    label: "🇮🇹 Italiano",
+  },
+  {
+    lang: "nl",
+    label: "🇳🇱 Nederlands",
+  },
+  {
+    lang: "ko",
+    label: "🇰🇷 한국어",
+  },
+  // {
+  //   lang: "id",
+  //   label: "🇮🇩 Bahasa Indonesia",
+  // },
+  // {
+  //   lang: "tr",
+  //   label: "🇹🇷 Türkçe",
+  // },
+  // {
+  //   lang: "pl",
+  //   label: "🇵🇱 Polski",
+  // },
 ];
 
 export const AvailableLocales = Languages.map((l) => l.lang);
