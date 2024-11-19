30d354087472db1c8186558af3c840313e13dcd5
liuxin <2683961691@qq.com>
2024-11-19 14:13:23 +0800

    Update sites-group.tsx

--- packages/aigotools/src/components/common/sites-group.tsx
+++ packages/aigotools/src/components/common/sites-group.tsx
@@ -1,10 +1,10 @@
 import { Istok_Web } from "next/font/google";
 import clsx from "clsx";
 
+import SiteCard from "./site-card";
+
 import { Site } from "@/models/site";
 import Container from "@/components/common/container";
-
-import SiteCard from "./site-card";
 
 const istokWeb = Istok_Web({
   subsets: ["latin"],
@@ -27,6 +27,7 @@ export default function SiteGroup({
   return (
     <Container className="mt-10 sm:mt-16" id={id}>
       <h2 className={clsx(istokWeb.className, "text-2xl font-bold")}>
+        {id === "featured" ? <span>🔥</span> : ""}
         {title}
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
